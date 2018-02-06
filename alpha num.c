d027fd81d7ef76a55f9e30191d7d7300f77d186d
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-02-06 15:07:08 +0530

    Create alpha num.c

---
+++ alpha num.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("ONE");
+break;
+case 2:
+printf("TWO");
+break;
+case 3:
+printf("THREE");
+break;
+case 4:
+printf("FOUR");
+break;
+case 5:
+printf("FIVE");
+break;
+case 6:
+printf("SIX");
+break;
+case 7:
+printf("SEVEN");
+break;
+case 8:
+printf("EIGHT");
+break;
+case 9:
+printf("NINE");
+break;
+case 10:
+printf("TEN");
+break;
+default:
+printf("CHOICE IS NOT MATCHED");
+}
+return 0;
+}
