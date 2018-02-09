2a82f6da0b745c7a3010967bdb3d3e9ecf8eb160
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-02-09 14:41:27 +0530

    Create countch.c

---
+++ countch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[50];
+    int b,count=0,i;
+    scanf("%[^\n]s",a);
+    b=strlen(a);
+    for(i=0;i<b;i++)
+    {
+        if(a[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",b-count);
+    return 0;
+}
