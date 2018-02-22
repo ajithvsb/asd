13adbd0096afe8e1afc4143f578aff4f37601b8a
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-02-22 10:48:29 +0000

    Create neareven.c

---
+++ neareven.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+                scanf("%d",&a);
+		if(a%2==0)
+		{
+			printf("equal even :%d",a);
+		}
+		else
+		{
+			printf("nearest less then even:%d",a-1);
+		}
+	
+
+	
+}
