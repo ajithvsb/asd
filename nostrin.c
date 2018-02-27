c3cf9246ca8870e2f78c66af567837e0cde48df9
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-02-27 10:26:33 +0000

    Create nostrin.c

---
+++ nostrin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[50];
+    int b,i;
+    scanf("%s",&a);
+    scanf("\t%d",&b);
+    for(i=0;i<b;i++)
+    {
+    	printf("%c",a[i]);
+    
+   } return 0;
+}
+  
