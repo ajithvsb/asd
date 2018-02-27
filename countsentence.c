3536cbf4dbe36052840536002b2c058c512b2d8a
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-02-27 10:30:05 +0000

    Create countsentence.c

---
+++ countsentence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[50];
+    int b,count=1,i;
+    scanf("%[^\n]s",a);
+    b=strlen(a);
+    for(i=0;i<b;i++)
+    {
+        if(a[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}  
+   
