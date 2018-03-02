aaf619ce04742503e2df982c016fcf9fd151b130
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-03-02 11:19:15 +0000

    Create persquare.c

---
+++ persquare.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d\t%d",&a,&b);
+    if(a==b)
+    {
+    	printf("perfect square");
+    }
+    else
+    {
+    	printf("not perfect square");	
+    }
+    return 0;
+}
