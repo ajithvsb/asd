47a5389c5688a46b890b5ef465107ba9e1bcada6
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-03-02 11:09:50 +0000

    Create bwno.c

---
+++ bwno.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int a,b,n;
+    scanf("%d\t%d\t%d",&a,&b,&n);
+    if(n>=a&&n<=b)
+    {
+    	printf("present B\W two num");
+    }
+    else
+    {
+        printf(" not present B\W two num");	
+    }
+  
+    return 0;
+}
