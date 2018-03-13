c3c84146ac769666fd6c3504c23f68c5e50d9818
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2018-03-13 16:43:54 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1, num2, gcd, lcm, remainder, numerator, denominator;
+
+ 
+
+    printf("Enter two numbers\n");
+
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2)
+
+    {
+
+        numerator = num1;
+
+        denominator = num2;
+
+    }
+
+    else
+
+    {
+
+        numerator = num2;
+
+        denominator = num1;
+
+    }
+
+    remainder = numerator % denominator;
+
+    while (remainder != 0)
+
+    {
+
+        numerator   = denominator;
+
+        denominator = remainder;
+
+        remainder   = numerator % denominator;
+
+    }
+
+    gcd = denominator;
+
+    lcm = num1 * num2 / gcd;
+
+    printf("LCM of %d and %d = %d\n", num1, num2, lcm); return 0;
+}
+
